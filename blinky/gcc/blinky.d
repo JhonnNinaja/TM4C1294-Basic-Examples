4918401becf142760897675c400dadffb1333802
Jhonn <105321843+JhonnNinaja@users.noreply.github.com>
2024-02-28 14:39:06 -0400

    Add files via upload

---
+++ blinky/gcc/blinky.d
@@ -0,0 +1,11 @@
+gcc/blinky.o: blinky.c \
+ /usr/share/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ /usr/share/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ ../../../../inc/hw_memmap.h ../../../../driverlib/debug.h \
+ ../../../../driverlib/gpio.h ../../../../driverlib/sysctl.h
